353309f5e468f0a32c08c9ee8fdd73413ea92ad2
Nia Bert <nbert14@gmail.com>
2019-05-03 01:24:52 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,10 +14,9 @@ def line(array)
   end 
 end 
 
-def take_a_number(deli, name)
+def take_a_number(array, name)
     array.push(name)
   position = array.index(name) + 1
-
-  return name, position 
+    return #{name} , position 
 end
 
