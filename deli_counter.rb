219b511acecae88f8ab9e2b23fc4d38a892beb85
Nia Bert <nbert14@gmail.com>
2019-05-03 01:18:38 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,23 @@
-# Write your code here.
+katz_deli = []
+
+def line(array)
+  if array.length == 0
+    puts "The line is currently empty."
+    
+  else 
+    message = "The line is currently:"
+    
+  array.each_with_index do |value, index| 
+    message += " #{index.to_i+1}. #{value}"
+  end 
+  puts "#{message}"
+  end 
+end 
+
+def take_a_number(deli, name)
+   counter == index + 1
+  deli.each do |name|
+    deli.push(#{counter} . #{name})
+    counter += 1
+  end
+end
